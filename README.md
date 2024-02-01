d1aec708ceca9627c891e10bb53763477274adfa
XIVN1987 <XIVN1987@163.com>
2024-02-01 13:48:36 +0800

    add png2c

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ convert binary file (\*.bin, \*.jpg, \*.jpeg) content to c array.
 
 ## jpeg2c
 decode jpeg image and then save the decoded data to c array.
+
+## png2c
+decode png image and then save the decoded data to c array.
---
+++ png2c/png2c.py
@@ -0,0 +1,38 @@
+#!python3
+import os
+import numpy as np
+import pylab as pl
+
+
+def png2c(path, name):
+    img = pl.imread(path) * 255
+    img = img.astype(np.uint8)
+
+    height, width, _ = img.shape
+
+    img = img.reshape((-1, 4))
+
+    rgb888 = [(a << 24) | (r << 16) | (g << 8) | b for (r, g, b, a) in img]
+
+    with open(f'{name}_rgb888.h', 'w') as f:
+        f.write(f'const uint32_t Image_{name}_rgb888[{height}][{width}] = {{\n')
+        for i, x in enumerate(rgb888):
+            if i % 16 == 0 and i != 0:
+                f.write('\n')
+            f.write('0x%08X, ' %x)
+        f.write('\n};')
+    
+    rgb565 = [(a << 16) | ((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3) for (r, g, b, a) in img]
+    
+    with open(f'{name}_rgb565.h', 'w') as f:
+        f.write(f'const uint32_t Image_{name}_rgb565[{height}][{width}] = {{\n')
+        for i, x in enumerate(rgb565):
+            if i % 16 == 0 and i != 0:
+                f.write('\n')
+            f.write('0x%06X, ' %x)
+        f.write('\n};')
+
+
+for name in os.listdir('.'):
+    if name.endswith('.png'):
+        png2c(os.path.join(os.getcwd(), name), name.split('.')[0])
